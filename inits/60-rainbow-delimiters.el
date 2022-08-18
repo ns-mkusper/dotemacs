0d3bb0a7946979f25b6a66505e0e8cd1d09c1e2f
marek kusper <mkusper@bydlosoft.com>
2022-08-18 12:15:41 +0200

    colorize delimiters with sequential rainbow colors

---
+++ inits/60-rainbow-delimiters.el
@@ -0,0 +1,6 @@
+(use-package rainbow-delimiters
+  :ensure t
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
+
+(provide '60-rainbow-delimiters)
---
+++ inits/60-restclient.el
@@ -0,0 +1,4 @@
+(use-package restclient
+  :mode ("\\.http\\'" . restclient-mode))
+
+(provide '60-restclient)
