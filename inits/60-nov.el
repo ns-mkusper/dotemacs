f5bfd3cdea9fd12c729fd5d3e1dd5bbb516ec0cb
Marek Kusper <mkusper@bydlosoft.com>
2022-09-12 11:09:11 -0500

    Add epub support

---
+++ inits/60-nov.el
@@ -0,0 +1,16 @@
+;; Read epubs in emacs
+(use-package nov
+  :ensure t
+  :preface
+  (defun my/nov-delayed-render-setup ()
+    (run-with-idle-timer 0.2 nil 'nov-render-document))
+  (defun my/nov-fringes-setup ()
+    "Hide the fringes for `nov-mode'."
+    (set-window-fringes (get-buffer-window) 0 0 nil))
+  :mode
+  ("\\.epub$" . nov-mode)
+  :hook
+  (nov-mode . my/nov-delayed-render-setup)
+  (nov-mode . my/nov-fringes-setup))
+
+(provide '60-nov)
