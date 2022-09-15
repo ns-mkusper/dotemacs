669d406555bcda8a08b2d19f14a6f2dbbe2d3462
Marek Kusper <mkusper@bydlosoft.com>
2022-09-14 21:15:39 -0500

    drop cargo minor mode in favor of handling cargo via rustic

--- inits/60-rust.el
+++ inits/60-rust.el
@@ -4,55 +4,52 @@
   :after (flycheck lsp-mode)
   :mode
   ("\\.rs\\'" . rustic-mode)
+  :preface
+  ;; (defun my/rustic-mode-hook-fn ()
+  ;;   "Needed for lsp-format-buffer to indent with 4 spaces"
+  ;;   (setq tab-width 4
+  ;; 	  indent-tabs-mode nil))
+  (defun my/switch-to-cargo-window ()
+    "Switch to the *cargo* window."
+    (other-window 1))
+  (defun my/rustic-mode-hook-fn ()
+    "Makes running C-c C-c C-r without having to confirm"
+    (setq-local buffer-save-without-query t))
+  :init
+  ;; to use rustic-mode even if rust-mode also installed
+  (setq auto-mode-alist (delete '("\\.rs\\'" . rust-mode) auto-mode-alist)
+        rustic-lsp-server 'rust-analyzer)
   :hook
   (rustic-mode . global-linum-mode) ;; line display
   (rustic-mode . hs-minor-mode) ;; fold mode
   (rustic-mode . eldoc-mode) ;; code tracing
   (rustic-mode . company-mode) ;; autocomplete
-  (rustic-mode . cargo-minor-mode)
+  ;; (rustic-mode . cargo-minor-mode)
   (rustic-mode . lsp-mode)
   (rustic-mode . flycheck-mode)
   ;; (flycheck-mode . flycheck-rust-setup)
   (rustic-mode . (lambda () (setq indent-tabs-mode nil))) ;; set indent
-  :init
+  (rustic-mode . my/rustic-mode-hook-fn)
+  :config
   (setq rustic-lsp-server 'rust-analyzer
         rustic-lsp-client 'lsp-mode
         rustic-format-on-save t)
-  )
-
-(use-package cargo
-  :ensure t
-  :commands (cargo-minor-mode)
-  :if (executable-find "cargo")
-  :after (rust-mode)
-  :init
-  (add-hook 'rust-mode-hook 'cargo-minor-mode)
-  :config
-  (setq cargo-process--command-bench "cargo bench")
-  (setq cargo-process--command-build "cargo build")
-  (setq cargo-process--command-clean "cargo clean")
-  (setq cargo-process--command-doc "cargo doc")
-  (setq cargo-process--command-doc-open "cargo doc --open")
-  (setq cargo-process--command-new "cargo new")
-  (setq cargo-process--command-init "cargo init")
-  (setq cargo-process--command-run "cargo run")
-  (setq cargo-process--command-run-bin "cargo run --bin")
-  (setq cargo-process--command-run-example "cargo run --example")
-  (setq cargo-process--command-search "cargo search")
-  (setq cargo-process--command-test "cargo test")
-  (setq cargo-process--command-current-test "cargo test")
-  (setq cargo-process--command-current-file-tests "cargo test")
-  (setq cargo-process--command-update "cargo update")
-  (if (and (executable-find "cargo-fmt") (executable-find "rustfmt"))
-      ;; cargo install rustfmt
-      (setq cargo-process--command-fmt "cargo fmt"))
-  (if (and (executable-find "cargo-check"))
-      ;; cargo install cargo-check
-      (setq cargo-process--command-check "cargo check"))
-  (if (and (executable-find "cargo-clippy") (executable-find "clippy-driver"))
-      ;; cargo install clippy
-      (setq cargo-process--command-clippy "cargo clippy"))
-  )
-
+  (advice-add 'rustic-cargo-check :after #'my/switch-to-cargo-window)
+  (advice-add 'rustic-cargo-run :after #'my/switch-to-cargo-window)
+  (advice-add 'rustic-cargo-build :after #'my/switch-to-cargo-window)
+  ;; (add-hook 'before-save-hook (lambda () (when (eq 'rust-mode major-mode)
+  ;; 					   (lsp-format-buffer))))
+  ;; (add-hook 'lsp-after-open-hook (lambda ()
+  ;; 				   (when (lsp-find-workspace 'rust-analyzer nil)
+  ;; 				     (lsp-rust-analyzer-inlay-hints-mode))))
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status)))
 
 (provide '60-rust)
