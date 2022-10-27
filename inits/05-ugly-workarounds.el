55dc2dff4eefc6a60e366d1ab45fe19e8ed8c590
Mark Kusper <mkusper@rigetti.com>
2022-10-27 09:06:06 -0700

    comment menu bar change

--- inits/05-ugly-workarounds.el
+++ inits/05-ugly-workarounds.el
@@ -7,7 +7,7 @@
       (add-to-list 'gnutls-trustfiles
 		   (expand-file-name
 		    "~/etc/tls/certificates/comodo.rsa.ca.intermediate.crt"))
-
+;; menu bar mode on mac loses 0 space
       (menu-bar-mode 1))
 ;; get meta key working on Mac
 (setq-default mac-option-modifier 'meta)
