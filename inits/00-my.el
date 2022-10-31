94a4c53eff2c723221967a2f355fec33d6e7386b
mark kusper <mkusper@bydlosoft.com>
2022-10-31 12:10:50 -0500

    remove emacs-28 specific lines

--- inits/00-my.el
+++ inits/00-my.el
@@ -37,7 +37,6 @@
     (call-interactively #'eval-last-sexp)))
 
 (my-load-path "~/.emacs.d/lisp")
-;;(my-load-path "/usr/share/emacs/site-lisp/")
 
 ;;; General Settings
 (setq inhibit-startup-message t)
@@ -72,12 +71,6 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq-default indent-tabs-mode nil) ;; only use space indenting
 (setq completion-cycle-threshold 3) ;; TAB cycle if there are only few candidates
-
-;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-;; (setq read-extended-command-predicate
-;;       #'command-completion-default-include-p)
-
 ;; Enable indentation+completion using the TAB key.
 ;; `completion-at-point' is often bound to M-TAB.
 (setq tab-always-indent 'complete)
