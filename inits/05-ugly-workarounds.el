906e4d2c049c8aa059c2d36d0b1dcbf1133bf633
mark kusper <mkusper@bydlosoft.com>
2022-11-25 15:09:47 -0600

    add powershell mode for windows

--- inits/05-ugly-workarounds.el
+++ inits/05-ugly-workarounds.el
@@ -7,15 +7,15 @@
       (add-to-list 'gnutls-trustfiles
 		   (expand-file-name
 		    "~/etc/tls/certificates/comodo.rsa.ca.intermediate.crt"))
-;; menu bar mode on mac loses 0 space
+      ;; menu bar mode on mac loses 0 space
       (menu-bar-mode 1))
-;; get meta key working on Mac
-(setq-default mac-option-modifier 'meta)
-)
+  ;; get meta key working on Mac
+  (setq-default mac-option-modifier 'meta)
+  )
 ;; Windows needed workarounds
 (if (eq system-type 'windows-nt)
     ;; Windows API is built on UTF-16
     (set-selection-coding-system 'utf-16-le)          ; correct
-)
+  )
 
 (provide '05-ugly-workarounds)
---
+++ inits/60-powershell.el
@@ -0,0 +1,13 @@
+(use-package powershell
+  ;; https://learn.microsoft.com/en-us/archive/blogs/dotnetinterop/run-powershell-as-a-shell-within-emacs
+  :if (eq system-type 'windows-nt)
+  :ensure t
+  :config
+  ;; Change default compile command for powershell
+  (add-hook 'powershell-mode-hook
+            (lambda ()
+              (set (make-local-variable 'compile-command)
+                   (format "powershell.exe -NoLogo -NonInteractive -Command \"& '%s'\""             (buffer-file-name)))))
+  )
+
+(provide '60-powershell)
