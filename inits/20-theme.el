95708e21f913f451d89bd820cbe124ea1a3f1fd7
Mark Kusper <mkusper@rigetti.com>
2022-12-15 22:32:38 +0100

    set org level colors

--- inits/20-theme.el
+++ inits/20-theme.el
@@ -24,6 +24,20 @@
 (global-hl-line-mode 1)
 (set-face-background hl-line-face "grey10")
 
+;; set org level colors
+(custom-theme-set-faces 'user
+                        `(org-level-1 ((t (:foreground "#bfe438"))))
+                        `(org-level-2 ((t (:foreground "#29AAA1"))))
+                        `(org-level-3 ((t (:foreground "#eec900"))))
+                        `(org-level-4 ((t (:foreground "#01fcff"))))
+                        `(org-level-5 ((t (:foreground "#00bbea"))))
+                        `(org-level-6 ((t (:foreground "#c66f19"))))
+                        `(org-level-7 ((t (:foreground "#11bfaa"))))
+                        `(org-level-8 ((t (:foreground "#735875"))))
+                        `(org-level-9 ((t (:foreground "#9d3d4d"))))
+                        `(org-level-10 ((t (:foreground "ff6503"))))                        
+                        )
+
 ;; powerline and doom require full icon pack
 ;; run M-x all-the-icons-install-fonts to enable (and then install the downloaded font files if on windows)
 ;; TODO: automate ^
