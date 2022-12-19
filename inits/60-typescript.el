1b2c100beddb6453867ded021fa5bb3e3a2646f2
Mark Kusper <mkusper@rigetti.com>
2022-12-19 23:41:02 +0100

    enable dap for typescript debugging

--- inits/60-typescript.el
+++ inits/60-typescript.el
@@ -3,6 +3,10 @@
   :mode "\\.ts\\'"
   :hook (typescript-mode . lsp-deferred)
   :config
-  (setq typescript-indent-level 2))
+  (setq typescript-indent-level 2)
+  ;; Set up Node debugging
+  (require 'dap-node)
+  (dap-node-setup) ;; Automatically installs Node debug adapter if needed
+  )
 
 (provide '60-typescript)
