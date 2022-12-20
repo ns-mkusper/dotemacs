496e76516b04deec20a3c0558e7daf5851d848ff
Mark Kusper <mkusper@rigetti.com>
2022-12-20 12:53:09 +0100

    add dap-mode in separate pkg

---
+++ inits/60-dap.el
@@ -0,0 +1,26 @@
+(use-package dap-mode
+  :ensure t
+  :after lsp-mode
+  :bind
+  (:map dap-mode-map
+        (("<f12>" . dap-debug)
+         ("<f8>" . dap-continue)
+         ("<f9>" . dap-next)
+         ("<M-f11>" . dap-step-in)
+         ("C-M-<f11>" . dap-step-out)
+         ("C-c b" . dap-breakpoint-toggle)))
+  ;; enable supported languages and modes
+  :hook ((after-init . dap-mode)
+         (dap-mode . dap-ui-mode)
+         (python-mode . (lambda () (require 'dap-python)))
+         (ruby-mode . (lambda () (require 'dap-ruby)))
+         (go-mode . (lambda () (require 'dap-go)))
+         (java-mode . (lambda () (require 'dap-java)))
+         ((c-mode c++-mode objc-mode swift) . (lambda () (require 'dap-lldb)))
+         (php-mode . (lambda () (require 'dap-php)))
+         (elixir-mode . (lambda () (require 'dap-elixir)))
+         ((typescript-mode js-mode js2-mode) . (lambda () (require 'dap-node)))
+         ;; ((typescript-mode js-mode js2-mode) . (lambda () (require 'dap-chrome)))
+         ))
+
+(provide '60-dap)
