796a74047f3d0b0e25ef6139ecb41597f70d767c
mark kusper <mkusper@bydlosoft.com>
2023-01-01 10:10:52 -0600

    disable counsel which throws weird errors on windows

--- inits/40-ivy.el
+++ inits/40-ivy.el
@@ -19,53 +19,55 @@
     (setq magit-completing-read-function 'ivy-completing-read))
   )
 
-(use-package counsel
-  :straight t
-  :diminish ivy-mode counsel-mode
-  :defines
-  (projectile-completion-system magit-completing-read-function)
-  :hook
-  (after-init . ivy-mode)
-  (ivy-mode . counsel-mode)
-  :bind
-  ("M-x" . 'counsel-M-x)
-  ("M-y" . 'counsel-yank-pop)
-  ("C-M-z" . 'counsel-fzf)
-  ("C-M-r" . 'counsel-recentf)
-  ("C-x C-b" . 'counsel-ibuffer)
-  ("C-x C-i" . 'counsel-semantic-or-imenu)
-  :custom
-  ;; Include recent files and bookmarks in `ivy-switch-buffer' (Cx b) list
-  (ivy-use-virtual-buffers t)
+;; (use-package counsel
+;;   :straight t
+;;   ;; for some reason this throws weird errors on windows
+;;   ;; TODO: investigate
+;;   :diminish ivy-mode counsel-mode
+;;   :defines
+;;   (projectile-completion-system magit-completing-read-function)
+;;   :hook
+;;   (after-init . ivy-mode)
+;;   (ivy-mode . counsel-mode)
+;;   :bind
+;;   ("M-x" . 'counsel-M-x)
+;;   ("M-y" . 'counsel-yank-pop)
+;;   ("C-M-z" . 'counsel-fzf)
+;;   ("C-M-r" . 'counsel-recentf)
+;;   ("C-x C-b" . 'counsel-ibuffer)
+;;   ("C-x C-i" . 'counsel-semantic-or-imenu)
+;;   :custom
+;;   ;; Include recent files and bookmarks in `ivy-switch-buffer' (Cx b) list
+;;   (ivy-use-virtual-buffers t)
 
-  ;; Wrap prompt
-  (ivy-truncate-lines nil)
+;;   ;; Wrap prompt
+;;   (ivy-truncate-lines nil)
 
-  ;; If `Cp' at the beginning of the list, move to the end of the list
-  (ivy-wrap t)
+;;   ;; If `Cp' at the beginning of the list, move to the end of the list
+;;   (ivy-wrap t)
 
-  ;; Do not show unnecessary files in `counsel-find-file'
-  (counsel-find-file-ignore-regexp (regexp-opt completion-ignored-extensions))
-  :config
-  (use-package ivy-hydra
-    :straight t
-    :custom
-    ;; Assign Mo to ivy-hydra-read-action
-    (ivy-read-action-function #'ivy-hydra-read-action)
-    )
-  )
+;;   ;; Do not show unnecessary files in `counsel-find-file'
+;;   ;; (counsel-find-file-ignore-regexp (regexp-opt completion-ignored-extensions))
+;;   :config
+;;   (use-package ivy-hydra
+;;     :straight t
+;;     :custom
+;;     ;; Assign Mo to ivy-hydra-read-action
+;;     (ivy-read-action-function #'ivy-hydra-read-action)
+;;     )
+;;   )
 
 (use-package all-the-icons-ivy-rich
   :straight t
-  :after (counsel-projectile)
+  ;; :after (counsel-projectile)
   :config
   ;; counsel-projectile-find-file
-  (plist-put all-the-icons-ivy-rich-display-transformers-list
-             'counsel-projectile-find-file
-             '(:columns
-               (
-                (ivy-read-file-transformer)
-                (ivy-rich-counsel-find-file-truename (:face font-lock-doc-face)))))
+  ;; (plist-put all-the-icons-ivy-rich-display-transformers-list
+  ;;            'counsel-projectile-find-file
+  ;;            '(:columns
+  ;;              (
+  ;;               (ivy-read-file-transformer)
+  ;;               (ivy-rich-counsel-find-file-truename (:face font-lock-doc-face)))))
   (all-the-icons-ivy-rich-mode 1)
   )
 
@@ -74,12 +76,12 @@
   :config
   (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
   ;; counsel-projectile-find-file
-  (plist-put ivy-rich-display-transformers-list
-             'counsel-projectile-find-file
-             '(:columns
-               (
-                (ivy-read-file-transformer)
-                (ivy-rich-counsel-find-file-truename (:face font-lock-doc-face)))))
+  ;; (plist-put ivy-rich-display-transformers-list
+  ;;            'counsel-projectile-find-file
+  ;;            '(:columns
+  ;;              (
+  ;;               (ivy-read-file-transformer)
+  ;;               (ivy-rich-counsel-find-file-truename (:face font-lock-doc-face)))))
   (ivy-rich-mode 1))
 
 (use-package swiper
@@ -90,18 +92,18 @@
   (swiper-include-line-number-in-search t)
   :config
   ;; Enable preview in counsel-imenu
-  (ivy-configure 'counsel-imenu
-    :update-fn 'auto)
+  ;; (ivy-configure 'counsel-imenu
+  ;;   :update-fn 'auto)
   )
 
 (use-package avy
   :straight t )
 
-(use-package counsel-projectile
-  :straight t
-  :config
-  (counsel-projectile-mode 1)
-  )
+;; (use-package counsel-projectile
+;;   :straight t
+;;   :config
+;;   (counsel-projectile-mode 1)
+;;   )
 
 (use-package ivy-yasnippet
   :straight t )
