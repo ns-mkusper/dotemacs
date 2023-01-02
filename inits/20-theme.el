96138d81f13ee3a1d209d43b82a2cbe9d61013a9
mark kusper <mkusper@bydlosoft.com>
2023-01-01 18:51:42 -0600

    enable emojis

--- inits/20-theme.el
+++ inits/20-theme.el
@@ -54,10 +54,16 @@
 ;; powerline and doom require full icon pack
 (use-package all-the-icons
   :if (display-graphic-p)
+  :straight t
   :commands all-the-icons-install-fonts
   :init
   (unless (find-font (font-spec :name "all-the-icons"))
-    (all-the-icons-install-fonts t)))
+    (all-the-icons-install-fonts t))
+  )
 
+(use-package emojify
+  :straight t
+  :init
+  (global-emojify-mode))
 
 (provide '20-theme)
