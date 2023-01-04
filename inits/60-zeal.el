a2c7412f1d0d34514e4ac22c1ea90c5cf8ddea67
mark kusper <mkusper@bydlosoft.com>
2023-01-04 01:52:28 -0600

    use rust zeal docs with rustic

--- inits/60-zeal.el
+++ inits/60-zeal.el
@@ -1,12 +1,13 @@
 (use-package zeal-at-point
-  :straight t
   :straight (zeal-at-point :host github
-                   :repo "ns-mkusper/zeal-at-point"
-                   :local-repo "ns-mkusper/zeal-at-point"
-                   :branch "master"
-                   )
+                           :repo "ns-mkusper/zeal-at-point"
+                           :local-repo "zeal-at-point"
+                           :branch "master"
+                           )
   :bind
   ("\C-cz" . 'zeal-at-point)
+  :config
+  (add-to-list 'zeal-at-point-mode-alist '(rustic-mode . "rust"))
   )
 
 (provide '60-zeal)
