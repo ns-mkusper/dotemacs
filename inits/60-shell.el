c436c0fb65476122b8008ab29b7dd4d1670dfd78
mark kusper <mkusper@bydlosoft.com>
2023-01-04 01:45:25 -0600

    re-enable powershell mode

--- inits/60-shell.el
+++ inits/60-shell.el
@@ -43,21 +43,21 @@
 
 
 ;; probably better to use powershell on windows
-;; (use-package powershell
-;;   ;; https://learn.microsoft.com/en-us/archive/blogs/dotnetinterop/run-powershell-as-a-shell-within-emacs
-;;   :if (eq system-type 'windows-nt)
-;;   :straight t
-;;   :bind (("C-c RET" . open-powershell-with-project-buffer-name))
-;;   :config
-;;   (defun open-powershell-with-project-buffer-name ()
-;;     (interactive)
-;;     (setq shell-buffer-name (my-get-shell-buffer-name))
-;;     (powershell shell-buffer-name nil))
-;;   ;; Change default compile command for powershell
-;;   ;; (add-hook 'powershell-mode-hook
-;;   ;;           (lambda ()
-;;   ;;             (set (make-local-variable 'compile-command)
-;;   ;;                  (format "powershell.exe -NoLogo -NonInteractive -Command \"& '%s'\""             ((my-get-project-or-filename) )))))
-;;   )
+(use-package powershell
+  ;; https://learn.microsoft.com/en-us/archive/blogs/dotnetinterop/run-powershell-as-a-shell-within-emacs
+  :if (eq system-type 'windows-nt)
+  ;; :straight t
+  ;; :bind (("C-c RET" . open-powershell-with-project-buffer-name))
+  ;; :config
+  ;; (defun open-powershell-with-project-buffer-name ()
+  ;;   (interactive)
+  ;;   (setq shell-buffer-name (my-get-shell-buffer-name))
+  ;;   (powershell shell-buffer-name nil))
+  ;; Change default compile command for powershell
+  ;; (add-hook 'powershell-mode-hook
+  ;;           (lambda ()
+  ;;             (set (make-local-variable 'compile-command)
+  ;;                  (format "powershell.exe -NoLogo -NonInteractive -Command \"& '%s'\""             ((my-get-project-or-filename) )))))
+  )
 
 (provide '60-shell)
--- inits/60-zeal.el
+++ inits/60-zeal.el
@@ -1,6 +1,6 @@
 (use-package zeal-at-point
   :straight t
-  :straight (shfmt :host github
+  :straight (zeal-at-point :host github
                    :repo "ns-mkusper/zeal-at-point"
                    :local-repo "ns-mkusper/zeal-at-point"
                    :branch "master"
