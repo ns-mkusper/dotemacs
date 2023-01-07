9af09667d7b54ac59481531abf4575bbcc1be7ad
Mark Kusper <mkusper@rigetti.com>
2023-01-07 01:23:20 +0100

    add gitlab-ci

---
+++ inits/60-gitlab-ci.el
@@ -0,0 +1,11 @@
+(use-package gitlab-ci-mode
+  :straight t
+  :mode "\\.gitlab-ci.yml\\'")
+
+(use-package gitlab-ci-mode-flycheck
+  :straight t
+  :after flycheck gitlab-ci-mode
+  :init
+  (gitlab-ci-mode-flycheck-enable))
+
+(provide 60-gitlab-ci)
