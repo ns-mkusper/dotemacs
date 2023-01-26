16ecc98827ec7ed7a453ef45cd054545fb57c85d
Mark Kusper <mkusper@rigetti.com>
2023-01-27 00:39:21 +0100

    adjust fci column to 100 line width

--- inits/20-fill-column-indicator.el
+++ inits/20-fill-column-indicator.el
@@ -1,8 +1,9 @@
 ;; visual indenting
 (use-package fill-column-indicator
   :straight t
+  :commands (fci-mode)
   :init
-  (setq fci-rule-column 80)
+  (setq fci-rule-column 100)
   (setq fci-rule-use-dashes t)
   (setq fci-dash-pattern 0.5)
   (setq fci-rule-width 1)
