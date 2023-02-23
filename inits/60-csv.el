1100af883e755f3ffede37e40721883bf4a84d65
Mark Kusper <mkusper@rigetti.com>
2023-02-24 00:49:33 +0100

    make header show constantly in csv mode

--- inits/60-csv.el
+++ inits/60-csv.el
@@ -1,6 +1,10 @@
 (use-package csv-mode
   :straight t
   ;; Always enter CSV mode in align mode; makes it easier to read.
-  :hook (csv-mode . csv-align-mode))
+  :hook (csv-mode . csv-align-mode)
+  :init
+  ;; show persistent header line
+  (csv-header-line)
+)
 
 (provide '60-csv)
