7dea8fac386daa6114f6ec55e24fd9010389322f
Mark Kusper <mkusper@rigetti.com>
2024-01-10 00:25:35 +0100

    add go

---
+++ inits/60-go.el
@@ -0,0 +1,16 @@
+(defun my-go-before-save ()
+  (interactive)
+  (when lsp-mode
+    (lsp-organize-imports)
+    (lsp-format-buffer)))
+
+(use-package go-mode
+  :defer t
+  :straight t
+  :config
+  (add-hook 'go-mode-hook 'lsp-deferred)
+  (add-hook 'go-mode-hook
+            (lambda ()
+              (add-hook 'before-save-hook 'my-go-before-save))))
+
+(provide '60-go)
