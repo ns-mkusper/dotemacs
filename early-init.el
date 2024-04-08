05ee10c098d08d9653b223aeaa7d479775801b33
mark kusper <mkusper@bydlosoft.com>
2024-04-08 05:16:49 +0200

    add workaround for native-comp bug on windblows

--- early-init.el
+++ early-init.el
@@ -1,2 +1,5 @@
 ;; prevent package.el loading packages prior to their init-file loading
 (setq package-enable-at-startup nil)
+;; workaround for straight bug
+;; see: https://jeffkreeftmeijer.com/emacs-native-comp-log/
+(defvar native-comp-deferred-compilation-deny-list nil)
