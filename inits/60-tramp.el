9269d7340493846a0b1e811eb2efccc336989fc9
Mark Kusper <mkusper@rigetti.com>
2024-05-08 17:33:34 +0200

    enable tramp workaround to load straight-built version over system version

--- inits/60-tramp.el
+++ inits/60-tramp.el
@@ -1,21 +1,55 @@
 (use-package tramp
-  :straight nil
-  :custom
-  (tramp-default-method "ssh")
-  ;; cache file names for 10 seconds
-  (remote-file-name-inhibit-cache 10)
-
-  ;; do not store remote command history over tramp
-  (tramp-histfile-override t)
-
+  :straight (tramp :type git
+                   :repo "https://git.savannah.gnu.org/git/tramp.git"
+                   :host nil
+                   :pre-build
+                   (("autoconf")
+                    ("./configure")
+                    ("make")))
+  :demand t
+ ;; :init
+ ;; (autoload #'tramp-register-crypt-file-name-handler "tramp-crypt")
   :config
+  ;;(setq tramp-verbose 6)
   (defun my-turn-off-project-detection ()
     ;; projectile causes slowness over remote connection
     ;; see: https://www.reddit.com/r/emacs/comments/xul3qm/comment/iqy0gct/?utm_source=reddit&utm_medium=web2x&context=3
     (setq-local projectile-auto-update-cache nil)
     (setq-local projectile-dynamic-mode-line nil)
     (setq-local doom-modeline-project-detection nil))
-  (add-hook 'tramp-mode-hook #'my-turn-off-project-detection))
+  (add-hook 'tramp-mode-hook #'my-turn-off-project-detection)
+
+  (setq tramp-default-method "ssh")
+  (setq tramp-shell-prompt-pattern "\\(?:^\\|\r\\)[^]#$%>\n]*#?[]#$%>].* *\\(^[\\[[0-9;]*[a-zA-Z] *\\)*")
+
+  (setq vc-ignore-dir-regexp
+        (format "\\(%s\\)\\|\\(%s\\)"
+                vc-ignore-dir-regexp
+                tramp-file-name-regexp))
+
+  ;; Honor remote PATH.
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+
+  ;; cache file names for 10 seconds
+  ;; (remote-file-name-inhibit-cache 10)
+
+
+  ;; Allow ssh connections to persist.
+  ;;
+  ;; This seems to maybe cause tramp to hang a lot.
+  (customize-set-variable 'tramp-use-ssh-controlmaster-options nil)
+  ;; (customize-set-variable 'tramp-use-ssh-controlmaster-options t)
+
+  (if (eq window-system 'w32)
+                                        ;(setq tramp-default-method "ssh")
+      (setq tramp-default-method "scp")
+                                        ;(setq tramp-default-method "scpx")
+    (setq tramp-default-method "ssh"))
+
+  ;;(setq tramp-default-method "ssh")
+  ;;(tramp-change-syntax 'simplified)
+  ;;(setq tramp-verbose 10)
+  )
 
 (use-package tramp-sh
   :straight nil
