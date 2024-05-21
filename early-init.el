302ad7a9028524db7d016359dbe4b154c8206938
Mark Kusper <mkusper@rigetti.com>
2024-05-21 03:00:22 +0200

    remove comments

--- early-init.el
+++ early-init.el
@@ -3,5 +3,3 @@
 ;; workaround for straight bug
 ;; see: https://jeffkreeftmeijer.com/emacs-native-comp-log/
 (defvar native-comp-deferred-compilation-deny-list nil)
-;; No titlebar on emacs 29 and 30
-;; see: https://emacs.stackexchange.com/questions/62258/undecorated-frame-with-header-line-as-titlebar
