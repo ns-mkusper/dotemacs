02825337d159fc42feaa2a728aa71cc361431fab
mark kusper <mkusper@bydlosoft.com>
2024-05-29 20:26:37 -0500

    set leetcode path

--- inits/60-competitive-programming.el
+++ inits/60-competitive-programming.el
@@ -2,6 +2,7 @@
   :straight (gpt :type git :host github :repo "ginqi7/leetcode-emacs")
   :config
   (setq leetcode-language "python")
+  (setq leetcode-path "~/git/leetcode")
   )
 
 (provide '60-competitive-programming)
