c793a59569fa162c814fe65c5621f278b44dde97
mark kusper <mkusper@bydlosoft.com>
2024-06-06 19:21:03 -0500

    set default major modes for atomic chrome sites

--- inits/60-chrome-emacs.el
+++ inits/60-chrome-emacs.el
@@ -6,6 +6,12 @@
              :host github)
   :commands (atomic-chrome-start-server)
   :config
+  (setq-default atomic-chrome-buffer-open-style 'frame)
+  (setq-default atomic-chrome-auto-remove-file t)
+  (setq-default atomic-chrome-url-major-mode-alist
+                '(("github.com" . gfm-mode)
+                  ("gitlab.com" . gfm-mode)
+                  ("leetcode.com" . python-mode)))
   (setq-default atomic-chrome-extension-type-list '(atomic-chrome))
   (atomic-chrome-start-server))
 
