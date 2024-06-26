d6cda873a8a19847883d52edb119c041677b8310
Mark Kusper <mkusper@rigetti.com>
2024-06-26 19:54:48 +0200

    unable visualizer diff in undo-tree

--- inits/60-undo-tree.el
+++ inits/60-undo-tree.el
@@ -1,9 +1,11 @@
 (use-package undo-tree
   :straight t
-  :config
-  (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
-  (setq undo-tree-auto-save-history t)
   :init
-  (global-undo-tree-mode))
+  (global-undo-tree-mode)
+  :custom
+  (undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
+  (undo-tree-auto-save-history t)
+  (undo-tree-visualizer-diff t)
+  (undo-tree-visualizer-timestamps t))
 
 (provide '60-undo-tree)
