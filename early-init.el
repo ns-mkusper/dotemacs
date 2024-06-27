4ec8232ba395965fed408999dbd5c94d08e08fd8
mark kusper <mkusper@bydlosoft.com>
2024-06-27 15:35:00 -0500

    enable plist for lsp optimization

--- early-init.el
+++ early-init.el
@@ -3,3 +3,6 @@
 ;; workaround for straight bug
 ;; see: https://jeffkreeftmeijer.com/emacs-native-comp-log/
 (defvar native-comp-deferred-compilation-deny-list nil)
+;; lsp-mode optimization
+;; see: https://emacs-lsp.github.io/lsp-mode/page/performance/#use-plists-for-deserialization
+(setenv "LSP_USE_PLISTS" "true")
