40e0b36d4e3e48689362ecf1d7a7cc52ccd482ca
mark kusper <mkusper@bydlosoft.com>
2024-06-27 21:28:27 -0500

    increase company idle delay

--- inits/40-company.el
+++ inits/40-company.el
@@ -17,7 +17,7 @@
 
   (setq
    company-echo-delay 0
-   company-idle-delay 0.2
+   company-idle-delay 0.3
    company-minimum-prefix-length 1
    company-tooltip-align-annotations t
    company-tooltip-limit 20)
