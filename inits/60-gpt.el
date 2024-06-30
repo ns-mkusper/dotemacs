26b715951d49e8b02f659f9cf18a98d169ead8ad
mark kusper <mkusper@bydlosoft.com>
2024-06-30 14:10:13 -0500

    add claude

--- inits/60-gpt.el
+++ inits/60-gpt.el
@@ -5,6 +5,19 @@
 ;; allows code generation from gpt
 (use-package codegpt
   :straight (codegpt :type git :host github :repo "emacs-openai/codegpt"))
+;; Setup claude
+(require 'auth-source)
+
+(defun my/gptel-api-key (host)
+  (let ((secret (auth-source-pick-first-password
+                 :host host)))
+    secret))
+
+(setq
+ gptel-model "claude-3-sonnet-20240229" ;  "claude-3-opus-20240229" also available
+ gptel-backend (gptel-make-anthropic "Claude"
+                 :stream t :key (my/gptel-api-key "api.claude.com")))
+
 
 ;; TODO: add support for gemini?
 
