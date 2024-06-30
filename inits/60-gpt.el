820e8c03f8cfe70b4c278fd8cb0e01531d5a59a0
mark kusper <mkusper@bydlosoft.com>
2024-06-30 14:11:21 -0500

    cleanup

--- inits/60-gpt.el
+++ inits/60-gpt.el
@@ -1,23 +1,22 @@
-;; in-emacs gpt prompt
-(use-package gptel
-  :straight (gpt :type git :host github :repo "karthink/gptel"))
-
-;; allows code generation from gpt
-(use-package codegpt
-  :straight (codegpt :type git :host github :repo "emacs-openai/codegpt"))
-;; Setup claude
-(require 'auth-source)
-
 (defun my/gptel-api-key (host)
   (let ((secret (auth-source-pick-first-password
                  :host host)))
     secret))
+;; in-emacs gpt prompt
+(use-package gptel
+  :straight (gpt :type git :host github :repo "karthink/gptel")
+  :config
+  ;; Setup claude
+  (setq
+   gptel-model "claude-3-sonnet-20240229" ;  "claude-3-opus-20240229" also available
+   gptel-backend (gptel-make-anthropic "Claude"
+                   :stream t :key (my/gptel-api-key "api.claude.com")))
 
-(setq
- gptel-model "claude-3-sonnet-20240229" ;  "claude-3-opus-20240229" also available
- gptel-backend (gptel-make-anthropic "Claude"
-                 :stream t :key (my/gptel-api-key "api.claude.com")))
+  )
 
+;; allows code generation from gpt
+(use-package codegpt
+  :straight (codegpt :type git :host github :repo "emacs-openai/codegpt"))
 
 ;; TODO: add support for gemini?
 
