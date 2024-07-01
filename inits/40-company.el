ca2a3ebd9b7df4364f13911513b27e1827c9b933
mark kusper <mkusper@bydlosoft.com>
2024-07-01 17:40:37 -0500

    tune company

--- inits/40-company.el
+++ inits/40-company.el
@@ -18,11 +18,11 @@
 
   (setq
    company-echo-delay 0
-   company-idle-delay 0.5
+   company-idle-delay 0.2
    company-show-numbers t
    company-minimum-prefix-length 1
    company-tooltip-align-annotations t
-   company-minimum-prefix-length 0
+   company-minimum-prefix-length 1
    company-tooltip-limit 20)
   :bind
   (:map company-active-map
