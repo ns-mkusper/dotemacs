b1cb9528f2f9b50f9c442e9a5c38244171ba0ede
mark kusper <mkusper@bydlosoft.com>
2024-07-02 12:31:19 -0500

    add breadcrumb to replace lsp header

---
+++ inits/60-breadcrumb.el
@@ -0,0 +1,12 @@
+;;; Header line context of symbol/heading (breadcrumb.el)
+(use-package breadcrumb
+  :straight t
+  :hook (after-init . breadcrumb-mode)
+  :config
+  (setq breadcrumb-project-max-length 0.5)
+  (setq breadcrumb-project-crumb-separator "/")
+  (setq breadcrumb-imenu-max-length 10.0)
+  (setq breadcrumb-imenu-crumb-separator " » "))
+
+
+(provides '60-breadcrumb)
