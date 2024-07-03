b229dd143352ec2b90e83044447cc16e0974e3dc
mark kusper <mkusper@bydlosoft.com>
2024-07-02 21:25:30 -0500

    add eldoc-box

--- inits/40-eglot.el
+++ inits/40-eglot.el
@@ -3,12 +3,12 @@
   :straight t
   :bind (:map eglot-mode-map
               ("C-c h" . eldoc-doc-buffer)
-              ("C-c f r" . xref-find-references)
-              ("C-c f d" . eglot-find-declaration ;; xref-find-definitions
+              ("C-c l r" . xref-find-references)
+              ("C-c l d" . eglot-find-declaration ;; xref-find-definitions
                )
-              ("C-c f D" . xref-find-definitions-other-window)
-              ("C-c f t" . eglot-find-typeDefinition)
-              ("C-c f i" . eglot-find-implementation)
+              ("C-c l D" . xref-find-definitions-other-window)
+              ("C-c l t" . eglot-find-typeDefinition)
+              ("C-c l i" . eglot-find-implementation)
               ("C-c =" . eglot-format-buffer)
               ("C-c c" . eglot-completion-at-point)
               ("C-c r" . eglot-rename)
@@ -20,9 +20,15 @@
   (add-hook 'prog-mode-hook #'eglot-ensure)
 
   (setq eldoc-echo-area-use-multiline-p t)
-  (setq eldoc-documentation-function #'eldoc-documentation-compose)
+  (setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly)
   (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
   )
+
+(use-package eldoc-box
+  :straight (:build t)
+  :hook (prog-mode . eldoc-box-hover-at-point-mode)
+  :after eldoc)
+
 
 (use-package eglot-booster
   :straight (eglot-booster :type git :host github :repo "jdtsmith/eglot-booster")
