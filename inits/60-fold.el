c8037d259708efc2b607178137a461705de0e004
mark kusper <mkusper@bydlosoft.com>
2024-07-03 16:59:28 -0500

    disable ts-fold-indicators-mode because it's slow

--- inits/60-fold.el
+++ inits/60-fold.el
@@ -5,14 +5,16 @@
 (use-package ts-fold
   :straight (ts-fold :type git :host github :repo "emacs-tree-sitter/ts-fold")
   :config
-  (add-hook 'tree-sitter-after-on-hook #'ts-fold-indicators-mode)
+  ;; TODO: Figure out why this causes company to be insanely slow
+  ;; (add-hook 'tree-sitter-after-on-hook #'ts-fold-indicators-mode)
   (my-defkeymap
    "my-ts-fold" "C-c f"
    '("O" . ts-fold-open-all)
    '("o" . ts-fold-open-recursively)
    '("C" . ts-fold-close-all)
    '("c" . ts-fold-close)
-   '("z" . ts-fold-toggle)))
+   '("z" . ts-fold-toggle)
+   '("i" . ts-fold-indicators-mode)))
 
 
 
