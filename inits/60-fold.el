e75e108877a2ebd06f353b9f0eeaece7156aa7c8
mark kusper <mkusper@bydlosoft.com>
2024-07-02 21:25:47 -0500

    fix fold errors

--- inits/60-fold.el
+++ inits/60-fold.el
@@ -1,9 +1,11 @@
 ;; Code Folding
+(use-package ts-fold-indicators
+  :straight (ts-fold-indicators :type git :host github :repo "emacs-tree-sitter/ts-fold"))
+
 (use-package ts-fold
   :straight (ts-fold :type git :host github :repo "emacs-tree-sitter/ts-fold")
   :config
-  (global-ts-fold-mode)
-  (global-ts-fold-indicators-mode)
+  (add-hook 'tree-sitter-after-on-hook #'ts-fold-indicators-mode)
   (my-defkeymap
    "my-ts-fold" "C-c f"
    '("O" . ts-fold-open-all)
@@ -11,5 +13,7 @@
    '("C" . ts-fold-close-all)
    '("c" . ts-fold-close)
    '("z" . ts-fold-toggle)))
+
+
 
 (provide '60-fold)
