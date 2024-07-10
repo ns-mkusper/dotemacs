94063ba1529a083bdf739c717706e847d26aef69
mark kusper <mkusper@bydlosoft.com>
2024-07-10 15:32:47 -0500

    enable undo in region

--- inits/60-undo-tree.el
+++ inits/60-undo-tree.el
@@ -6,6 +6,8 @@
   (undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
   (undo-tree-auto-save-history t)
   (undo-tree-visualizer-diff t)
-  (undo-tree-visualizer-timestamps t))
+  (undo-tree-visualizer-timestamps t)
+  (undo-tree-enable-undo-in-region t))
+
 
 (provide '60-undo-tree)
