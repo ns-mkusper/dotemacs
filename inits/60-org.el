de2c1a447ac9f34425e048be887a67e43a939023
mark kusper <mkusper@bydlosoft.com>
2024-07-20 20:55:17 -0500

    add org-roam-ui

--- inits/60-org.el
+++ inits/60-org.el
@@ -245,5 +245,18 @@ Comments:
          ;; Dailies
          ("C-c n j" . org-roam-dailies-capture-today)))
 
+(use-package org-roam-ui
+  :straight t
+  :after org-roam ;; or :after org
+  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;;         a hookable mode anymore, you're advised to pick something yourself
+  ;;         if you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :custom
+  (org-roam-ui-sync-theme t)
+  (org-roam-ui-follow t)
+  (org-roam-ui-update-on-save t)
+  (org-roam-ui-open-on-start t))
+
 
 (provide '60-org)
