4c2ac32ff100dfa7c725d8c90fa1fb8e741ecf31
mark kusper <mkusper@bydlosoft.com>
2024-07-23 21:09:02 -0500

    disable-copilot

--- inits/60-copilot.el
+++ inits/60-copilot.el
@@ -1,13 +1,13 @@
-(use-package copilot
-  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
-  :hook (prog-mode . copilot-mode)
-  :general
-  ('insert copilot-mode-map
-           "C-f" 'copilot-accept-completion
-           "M-f" 'copilot-accept-completion-by-word
-           "C-e" 'copilot-accept-completion-by-line
-           "M-p" 'copilot-previous-completion
-           "M-n" 'copilot-next-completion))
+;; (use-package copilot
+;;   :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+;;   :hook (prog-mode . copilot-mode)
+;;   :general
+;;   ('insert copilot-mode-map
+;;            "C-f" 'copilot-accept-completion
+;;            "M-f" 'copilot-accept-completion-by-word
+;;            "C-e" 'copilot-accept-completion-by-line
+;;            "M-p" 'copilot-previous-completion
+;;            "M-n" 'copilot-next-completion))
 
 
 (provide '60-copilot)
