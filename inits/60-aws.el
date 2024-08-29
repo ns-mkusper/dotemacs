a4fc01f6b03f0c5cf2f9b714496b789392cf3738
Mark Kusper <mkusper@g-p.com>
2024-08-29 12:44:33 -0500

    add mode list match for cfn mode

--- inits/60-aws.el
+++ inits/60-aws.el
@@ -1,3 +1,15 @@
-(use-package cfn-mode)
+(use-package cfn-mode
+  :straight (cfn-mode
+             :repo "worr/cfn-mode"
+             :type git
+             :host gitlab)
+  :config
+  ;; (flycheck-cfn-setup)
+  (add-to-list 'magic-mode-alist
+    '("\\(.\\|\n\\)*Type: AWS::" . cfn-mode))
+  ;; hack to stop this file from having cfn-mode enabled based on the
+  ;; presence of the cfn-mode magic-mode-alist set up
+  (add-to-list 'magic-mode-alist
+    '("\\(.\\|\n\\)*;;; Commentary" . emacs-lisp-mode)))
 
 (provide '60-aws)
