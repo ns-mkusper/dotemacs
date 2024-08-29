34c080f3c6ca326727ad370fbf0a511f91217aae
Mark Kusper <mkusper@g-p.com>
2024-08-29 12:50:40 -0500

    call flycheck-cfn-setup automatically

--- inits/60-aws.el
+++ inits/60-aws.el
@@ -1,15 +1,15 @@
 (use-package cfn-mode
-  :straight (cfn-mode
-             :repo "worr/cfn-mode"
-             :type git
-             :host gitlab)
   :config
-  ;; (flycheck-cfn-setup)
   (add-to-list 'magic-mode-alist
     '("\\(.\\|\n\\)*Type: AWS::" . cfn-mode))
   ;; hack to stop this file from having cfn-mode enabled based on the
   ;; presence of the cfn-mode magic-mode-alist set up
   (add-to-list 'magic-mode-alist
     '("\\(.\\|\n\\)*;;; Commentary" . emacs-lisp-mode)))
+
+(use-package flycheck-cfn
+  :after (flycheck cfn-mode)
+  :config
+  (flycheck-cfn-setup))
 
 (provide '60-aws)
