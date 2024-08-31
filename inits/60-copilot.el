cac9c6d6cdb4a9a4caf884028a2bf4f111f078b1
Mark Kusper <mkusper@g-p.com>
2024-08-31 13:02:10 -0500

    add copilot

--- inits/60-copilot.el
+++ inits/60-copilot.el
@@ -1,13 +1,22 @@
-;; (use-package copilot
-;;   :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
-;;   :hook (prog-mode . copilot-mode)
-;;   :general
-;;   ('insert copilot-mode-map
-;;            "C-f" 'copilot-accept-completion
-;;            "M-f" 'copilot-accept-completion-by-word
-;;            "C-e" 'copilot-accept-completion-by-line
-;;            "M-p" 'copilot-previous-completion
-;;            "M-n" 'copilot-next-completion))
+(use-package copilot
+  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+  :hook (prog-mode . copilot-mode)
+  :general
+  ('insert copilot-mode-map
+           "C-f" 'copilot-accept-completion
+           "M-f" 'copilot-accept-completion-by-word
+           "C-e" 'copilot-accept-completion-by-line
+           "M-p" 'copilot-previous-completion
+           "M-n" 'copilot-next-completion))
 
+(use-package copilot-chat
+  :straight (:host github :repo "chep/copilot-chat.el" :files ("*.el"))
+  :after (request shell-maker)
+  :custom
+  (copilot-chat-frontend 'shell-maker)
+  :config
+  (require 'copilot-chat-shell-maker)
+  (push '(shell-maker . copilot-chat-shell-maker-init) copilot-chat-frontend-list)
+  (copilot-chat-shell-maker-init))
 
 (provide '60-copilot)
