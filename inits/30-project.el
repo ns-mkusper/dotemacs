ead7265ab39e253ae9566f728f6ddbdb1cb23bc5
Mark Kusper <mkusper@g-p.com>
2025-01-11 18:36:51 -0600

    add missing

--- inits/30-project.el
+++ inits/30-project.el
@@ -158,12 +158,28 @@ for one."
           (shell shell-buffer-name)
           (rename-buffer shell-buffer-name)))
       ))
-  :config
+
+  (defcustom project-root-markers
+    '("Cargo.toml" "compile_commands.json" "compile_flags.txt"
+      "project.clj" ".git" "deps.edn" "shadow-cljs.edn")
+    "Files or directories that indicate the root of a project."
+    :type '(repeat string)
+    :group 'project)
+
+  (defun project-root-p (path)
+    "Check if the current PATH has any of the project root markers."
+    (catch 'found
+      (dolist (marker project-root-markers)
+        (when (file-exists-p (concat path marker))
+          (throw 'found marker)))))
+
   (defun project-find-root (path)
     "Search up the PATH for `project-root-markers'."
     (when-let ((root (locate-dominating-file path #'project-root-p)))
       (cons 'transient (expand-file-name root))))
 
+
+  :config
   (add-to-list 'project-find-functions #'project-find-root)
   (bind-keys
    :map project-prefix-map
