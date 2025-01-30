e65fdc8677a9a103d4f8599675f32af80f1f3d30
Mark Kusper <mkusper@g-p.com>
2025-01-29 20:51:29 -0600

    disable copilot tab

--- inits/60-copilot.el
+++ inits/60-copilot.el
@@ -3,29 +3,31 @@
   :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
   :ensure t
   :bind (:map copilot-mode-map
-              ("<tab>" . my/copilot-tab)
+              ;; TODO: figure out a better way where this doesn't conflict with company
+              ;; ("<tab>" . my/copilot-tab)
               ("M-C-<" . copilot-previous-completion)
               ("M-C->" . copilot-next-completion)
               ("M-C-<tab>" . copilot-accept-completion-by-line))
   :config
 
-  (defun my/copilot-tab ()
-    "Call copilot if you can, else company else yasnippet, else indent."
-    (interactive)
-    (or (copilot-accept-completion)
-        (company-indent-or-complete-common nil)
-        ;; (company-yasnippet-or-completion)
-        (indent-for-tab-command nil))))
+  ;; (defun my/copilot-tab ()
+  ;;   "Call copilot if you can, else company else yasnippet, else indent."
+  ;;   (interactive)
+  ;;   (or (copilot-accept-completion)
+  ;;       (company-indent-or-complete-common nil)
+  ;;       ;; (company-yasnippet-or-completion)
+  ;;       (indent-for-tab-command nil))))
 
-;; disabled for being buggy
-;; (use-package copilot-chat
-;;   :straight (:host github :repo "chep/copilot-chat.el" :files ("*.el"))
-;;   :after (request shell-maker)
-;;   :custom
-;;   (copilot-chat-frontend 'shell-maker)
-;;   :config
-;;   (require 'copilot-chat-shell-maker)
-;;   (push '(shell-maker . copilot-chat-shell-maker-init) copilot-chat-frontend-list)
-;;   (copilot-chat-shell-maker-init))
+  ;; disabled for being buggy
+  ;; (use-package copilot-chat
+  ;;   :straight (:host github :repo "chep/copilot-chat.el" :files ("*.el"))
+  ;;   :after (request shell-maker)
+  ;;   :custom
+  ;;   (copilot-chat-frontend 'shell-maker)
+  ;;   :config
+  ;;   (require 'copilot-chat-shell-maker)
+  ;;   (push '(shell-maker . copilot-chat-shell-maker-init) copilot-chat-frontend-list)
+  ;; (copilot-chat-shell-maker-init)
+  )
 
 (provide '60-copilot)
