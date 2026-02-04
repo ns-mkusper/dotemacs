8edec7c4bc2f3782234941d6cb422aad9edbf859
Mark Kusper <mkusper@bydlosoft.com>
2026-02-04 16:18:39 -0600

    breadcrumb optimization delay

--- inits/60-breadcrumb.el
+++ inits/60-breadcrumb.el
@@ -6,7 +6,12 @@
   (setq breadcrumb-project-max-length 0.5)
   (setq breadcrumb-project-crumb-separator "/")
   (setq breadcrumb-imenu-max-length 1.0)
-  (setq breadcrumb-imenu-crumb-separator " » "))
+  (setq breadcrumb-imenu-crumb-separator " » ")
+
+  ;; Only update breadcrumbs when idle for 0.5s, not instantly.
+  ;; This prevents it from allocating strings during rapid undo/navigation.
+  (setq breadcrumb-idle-delay 0.5)
+  )
 
 
 (provide '60-breadcrumb)
